7d053c019221b93228d90d5698042fc26225b164
michael <dubatovmichael@gmail.com>
2022-03-25 17:03:00 +0300

    Empty application

---
+++ MobileUp Gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileUp Gallery
+//
+//  Created by Михаил on 25.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
